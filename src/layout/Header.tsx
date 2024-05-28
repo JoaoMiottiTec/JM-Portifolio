8fcd386530b359021a7466c1a9f305c4db273a32
Joao Vitor dos santos Miotti <joaovmiotti@gmail.com>
2024-05-28 14:51:46 -0300

    feat: css changes

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -45,10 +45,14 @@ const Header: React.FC<HeaderProps> = ({ theme, setTheme }) => {
     }, 300);
   };
 
+  const handleMenuClick = (setVisible: React.Dispatch<React.SetStateAction<boolean>>) => {
+    setVisible(false);
+  };
+
   return (
     <header ref={headerRef} className={`fixed top-0 w-full z-50 py-4 px-8`}>
       <div className="container mx-auto flex justify-center relative bg-transparent">
-        <div className={`absolute inset-0 rounded-full z-[-1] ${theme === 'light' ? 'bg-white text-black' : 'bg-black text-white'}`} style={{ width: '80%', margin: 'auto' }}></div>
+        <div className={`absolute inset-0 lg:w-5/6 rounded-full z-[-1] ${theme === 'light' ? 'bg-white text-black' : 'bg-black text-white'}`} style={{ width: '100%', margin: 'auto' }}></div>
         <div className="flex items-center justify-center space-x-4 rounded-full w-full lg:w-5/6 border p-4">
           <Image src={logo} height={75} width={75} alt='' />
           <div
@@ -63,12 +67,12 @@ const Header: React.FC<HeaderProps> = ({ theme, setTheme }) => {
                 onMouseLeave={() => hideMenu(setNavMenuVisible, navMenuTimeoutRef)}
                 className={`absolute mt-2 w-56 origin-top-left border border-gray-700 rounded-md shadow-lg focus:outline-none transition-transform transform ${theme === 'light' ? 'bg-white text-black' : 'bg-black text-white'}`}
               >
-                <div className="py-1">
+                <div className="py-1" onClick={() => handleMenuClick(setNavMenuVisible)}>
                   <a href="#hero" className="block px-4 py-2 text-sm hover:bg-gray-700">Tela Inicial</a>
                   <a href="#experience" className="block px-4 py-2 text-sm hover:bg-gray-700">Experiências</a>
                   <a href="#skills" className="block px-4 py-2 text-sm hover:bg-gray-700">Habilidades</a>
                   <a href="#projects" className="block px-4 py-2 text-sm hover:bg-gray-700">Projetos</a>
-                  <a href="#contact" className="block px-4 py-2 text-sm hover:bg-gray-700">Contato</a>
+                 {/*  <a href="#contact" className="block px-4 py-2 text-sm hover:bg-gray-700">Contato</a> */}
                 </div>
               </div>
             )}
@@ -85,7 +89,7 @@ const Header: React.FC<HeaderProps> = ({ theme, setTheme }) => {
                 onMouseLeave={() => hideMenu(setSocialMenuVisible, socialMenuTimeoutRef)}
                 className={`absolute mt-2 w-56 origin-top-left border border-gray-700 rounded-md shadow-lg focus:outline-none transition-transform transform ${theme === 'light' ? 'bg-white text-black' : 'bg-black text-white'}`}
               >
-                <div className="py-1">
+                <div className="py-1" onClick={() => handleMenuClick(setSocialMenuVisible)}>
                   <a
                     href="https://www.linkedin.com/in/joaovitormiotti/"
                     target="_blank"
@@ -122,13 +126,19 @@ const Header: React.FC<HeaderProps> = ({ theme, setTheme }) => {
               >
                 <div className="py-1">
                   <button
-                    onClick={() => toggleTheme('light')}
+                    onClick={() => {
+                      toggleTheme('light');
+                      handleMenuClick(setThemeMenuVisible);
+                    }}
                     className="block w-full px-4 py-2 text-sm flex items-center hover:bg-gray-700"
                   >
                     <SunIcon className="w-5 h-5 mr-2 text-black" />Claro
                   </button>
                   <button
-                    onClick={() => toggleTheme('dark')}
+                    onClick={() => {
+                      toggleTheme('dark');
+                      handleMenuClick(setThemeMenuVisible);
+                    }}
                     className="block w-full px-4 py-2 text-sm flex items-center hover:bg-gray-700"
                   >
                     <MoonIcon className="w-5 h-5 mr-2 text-white" />Escuro
